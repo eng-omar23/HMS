3151345f059931fafefdae02b0d57a5bde3a9cf8
Mohamedaweis32 <maxamedawayscumar32@gmail.com>
2023-08-03 17:25:44 +0300

    customer desing

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -1,55 +1,64 @@
+<?php // Get the username from the session
+$type = $_SESSION['type'];
+?>
 <!doctype html>
-<html lang="en"> 
-    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-    <head>     
-        <meta charset="utf-8" />
-        <title>HMS</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesbrand" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="../../../assets/images/favicon.ico">
-        <!-- Bootstrap Css -->
-        <link href="../../../assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="../../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="../../../assets/css/app.min.css" rel="stylesheet" type="text/css" />
-        <link rel="shortcut icon" href="../../../assets/images/favicon.ico">
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
-        <!-- DataTables -->
-        <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+<head>
+    <meta charset="utf-8" />
+    <title>HMS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../../../assets/images/favicon.ico">
+    <!-- Bootstrap Css -->
+    <link href="../../../assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="../../../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="../../../assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="../../../assets/images/favicon.ico">
 
-        <!-- Responsive datatable examples -->
-        <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />     
-    </head>
-    <body data-topbar="dark" data-layout="horizontal">
-        <!-- Begin page -->
-        <div id="layout-wrapper">
+    <!-- DataTables -->
+    <link href="assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
 
-            <header id="page-topbar">
-                <div class="navbar-header">
-                    
-                    <div class="d-flex">
-                    </div>
+    <!-- Responsive datatable examples -->
+    <link href="assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet"
+        type="text/css" />
+</head>
 
-                    <div class="d-flex">
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
+<body data-topbar="dark" data-layout="horizontal">
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+        <header id="page-topbar">
+            <div class="navbar-header">
+
+                <div class="d-flex">
+                </div>
+
+                <div class="d-flex">
+                    <div class="dropdown d-inline-block">
+                        <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img class="rounded-circle header-profile-user" src="../../../assets/images/users/avatar-1.jpg"
-                                    alt="Header Avatar">
-                                <span class="d-none d-xl-inline-block ms-1" key="t-henry">Henry</span>
-                                <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item text-danger" href="../../login.php"><i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span key="t-logout">Logout</span></a>
-                            </div>
+                            <img class="rounded-circle header-profile-user"
+                                src="../../../assets/images/users/avatar-1.jpg" alt="Header Avatar">
+                            <span class="d-none d-xl-inline-block ms-1" key="t-henry"><?php echo $type; ?></span>
+                            <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end">
+                            <!-- item-->
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item text-danger" href="../../../login.php"><i
+                                    class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span
+                                    key="t-logout">Logout</span></a>
                         </div>
                     </div>
-
                 </div>
-            </header>
+
+            </div>
+        </header>
--- application/views/customer/nav.php
+++ application/views/customer/nav.php
@@ -3,6 +3,54 @@
         <nav class="navbar navbar-light navbar-expand-lg topnav-menu">
             <div class="collapse navbar-collapse" id="topnav-menu-content">
                 <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-uielement" role="button">
+                            <i class="bx bx-tone me-2"></i>
+                            <span key="t-ui-elements">Profile</span>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-uielement" role="button">
+                            <i class="bx bx-tone me-2"></i>
+                            <span key="t-ui-elements"> Booking</span>
+                            <div class="arrow-down"></div>
+                        </a>
+
+                        <div class="dropdown-menu mega-dropdown-menu px-2 dropdown-mega-menu-sm"
+                            aria-labelledby="topnav-uielement">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div>
+
+                                        <a href="Booking.php" class="dropdown-item" key="t-buttons">View
+                                            History Booking</a>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-more" role="button">
+                            <i class="bx bx-file me-2"></i><span key="t-extra-pages">Reports</span>
+                            <div class="arrow-down"></div>
+                        </a>
+                        <div class="dropdown-menu mega-dropdown-menu px-2 dropdown-mega-menu-sm"
+                            aria-labelledby="topnav-uielement">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div>
+
+                                        <a href="Booking.php" class="dropdown-item" key="t-buttons">Hall Report</a>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </li>
                 </ul>
             </div>
         </nav>
